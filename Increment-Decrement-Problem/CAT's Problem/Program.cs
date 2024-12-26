88427ab9f5b87b1c2e2e0e280829947baf029e67
Sara Nabih <147654777+saraanbih@users.noreply.github.com>
2024-12-26 10:42:11 +0200

    Add files via upload

---
+++ Increment-Decrement-Problem/CAT's Problem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Problem
+{
+    class Program
+    {
+        static int IncrementDecrementOperations(List<string> operations) 
+        {
+            int result = 0;
+            foreach (string operation in operations) 
+            {
+                if (operation == "++") result++;
+                else if(operation == "--") result--;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter operations like (++,--) and" +
+                " put a space between every operation like (++ --) to make sure that results are correct:");
+            string op = Console.ReadLine();
+
+            if(op == string.Empty)
+                Console.WriteLine("You haven't entered anything");
+            else
+            {
+                List<string> operations = new List<string>(op.Split(' '));
+
+                int result = IncrementDecrementOperations(operations);
+
+                Console.WriteLine($"Final Result is: {result}");
+            }
+        }
+    }
+}
